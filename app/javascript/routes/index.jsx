2bf89b7f267135cbf51f4ac831aa3fcf0273488a
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-09-01 19:03:32 +0700

    add router configuration

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../components/Home";
-import Recipe from "./components/Recipe";
+import Recipe from "../components/Recipe";
 
 const Routes = () => {
   return (
